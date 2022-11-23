63e2cc3421ed9a116b826bcc31b20c7116cce61e
omniaatefmohamed <68192568+omniaatefmohamed@users.noreply.github.com>
2022-11-23 21:54:27 +0200

    Update DeleteData.js

--- src/components/DeleteData.js
+++ src/components/DeleteData.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
-const DeleteData = () => {
+const DeleteData = ({ dataList }) => {
     return (
         <div>
-            <Button className='mt-5 w-100' variant="danger">Delete All Data</Button>
+            {
+                dataList.legnth >= 1 ? (<Button className='mt-5 w-100' variant="danger">Delete All Data</Button>) : null
+            }
         </div>
     )
 }
