6e3231f6d416166aa5f2a4a432255dafc10dbfda
omniaatefmohamed <68192568+omniaatefmohamed@users.noreply.github.com>
2022-11-23 21:54:48 +0200

    Update Questions_list.js

--- src/components/Questions_list.js
+++ src/components/Questions_list.js
@@ -1,16 +1,25 @@
 import React from 'react'
-import { Accordion, Button } from 'react-bootstrap'
+import { Accordion, Button, Alert } from 'react-bootstrap'
 
-const Questions_list = () => {
+const Questions_list = ({ dataList }) => {
     return (
         <Accordion className='mt-3'>
-            <Accordion.Item eventKey="0">
-                <Accordion.Header>q1</Accordion.Header>
-                <Accordion.Body className='d-flex justify-content-between align-items-center'>
-                    <p className='mb-0'>kjhjkl</p>
-                    <Button variant="danger"> Delete</Button>
-                </Accordion.Body>
-            </Accordion.Item>
+            {
+                dataList.length >= 1 ? (() => {
+                    return (
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header>q1</Accordion.Header>
+                            <Accordion.Body className='d-flex justify-content-between align-items-center'>
+                                <p className='mb-0'>kjhjkl</p>
+                                <Button variant="danger"> Delete</Button>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    )
+                }) : (
+                    <Alert varient="info" className='mt-3'>
+                        No Questions to show
+                    </Alert>)
+            }
         </Accordion>
     )
 }
